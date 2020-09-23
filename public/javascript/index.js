a62051d16594892c6122db4df9b02bfafd102474
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-23 20:09:40 +0200

    Made the edit iteration 3 but it doesn't work and I don't get it, I am goign to do the create first

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -59,16 +59,29 @@ window.addEventListener('load', () => {
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
     event.preventDefault();
-
+    const inputValue = document.querySelector("input[name='character-id-delete']").value
+    charactersAPI.deleteOneRegister(inputValue).then((response) => {
+      console.log("response from API", response)}).catch((err) => {console.log(`There is an error :  ${err}`)})
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
     event.preventDefault();
+    const inputValue = document.querySelector("#edit-character-form input[name='chr-id']").value
+    
+    const nameValue = document.querySelector("#edit-character-form input[name='name']").value
+    const occupationValue = document.querySelector("#edit-character-form input[name='occupation']").value
+    const weaponValue = document.querySelector("#edit-character-form input[name='weapon']").value
+    const cartoonValue = document.querySelector("#edit-character-form input[name='cartoon']").value
+
+    const toEditValues = {nameValue,  occupationValue,  weaponValue, cartoonValue}
+    charactersAPI.updateOneRegister(inputValue, toEditValues).then((response) => {
+      console.log("response from API", response)}).catch((err) => {console.log(`There is an error :  ${err}`)})
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
     event.preventDefault();
+
 
   });
 });
