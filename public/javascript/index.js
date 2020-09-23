d6b84fba4fa19757f255b5aeea925d9ea6301292
Timothée Nicole <timothee.nicole@gmail.com>
2020-09-23 19:38:36 +0200

    Iteration 3 with fetch one done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -33,7 +33,28 @@ window.addEventListener('load', () => {
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
     event.preventDefault();
+    const inputValue = document.querySelector("input[name='character-id']").value
+    charactersAPI.getOneRegister(inputValue).then((response) => {
+      console.log("response from API", response);
+      const data = response.data;
 
+      let str = '';
+
+      str +=
+        `<div class="character-info">
+        <div class="name">${data.name}</div>
+        <div class="occupation">${data.occupation}</div>
+        <div class="cartoon">${data.cartoon}</div>
+        <div class="weapon">${data.weapon}</div>
+        </div>`
+      ;
+      
+      const charactersContainer = document.querySelector(".characters-container");
+      console.log(charactersContainer)
+      charactersContainer.innerHTML += str
+    }
+  )
+  .catch((err) => {console.log(`There is an error :  ${err}`)});
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
